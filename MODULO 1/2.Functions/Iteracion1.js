7404eeec307f4346bb00208fb596aec8eb9c06b0
Luismisito1609 <luismiguelgalanmunoz@gmail.com>
2024-03-22 14:52:06 +0100

    desarollo

---
+++ MODULO 1/2.Functions/Iteracion1.js
@@ -0,0 +1,9 @@
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne, numberTwo) {
+  if (numberTwo > numberOne) {
+    return numberTwo;
+  } else {
+    return numberOne;
+  }
+}
+console.log(sum(4, 10));
