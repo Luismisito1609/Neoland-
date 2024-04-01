0ac9bc29584ff591ad8d1eefc596f0d0eebaaee0
Luismisito1609 <luismiguelgalanmunoz@gmail.com>
2024-04-01 17:19:00 +0200

    desarollo

--- MODULO 1/2.Functions/Iteracion2.js
+++ MODULO 1/2.Functions/Iteracion2.js
@@ -1,7 +1,34 @@
-// Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings 
+// Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings
 // tenga la misma longitud deberá devolver el primero.
 // Puedes usar este array para probar tu:
-const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-function findLongestWord(param) {
-
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "CaptainA.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(arrayAvengers) {
+  let longestWord = "";
+  for (let i = 0; i < arrayAvengers.length; i++) {
+    if (longestWord < arrayAvengers[i].length) {
+      longestWord = arrayAvengers[i];
+    }
+  }
+  return longestWord;
 }
+let x = findLongestWord(avengers);
+console.log(x);
+
+// function findLongestWord(arrayAvengers) {
+//   let longestWord = "";
+//   for (let i = 0; i < arrayAvengers.length; i++) {
+//     if (longestWord.length < arrayAvengers[i].length) {
+//       longestWord = arrayAvengers[i];
+//     }
+//   }
+//   return longestWord;
+// }
+let b = findLongestWord(avengers);
+console.log(b);
