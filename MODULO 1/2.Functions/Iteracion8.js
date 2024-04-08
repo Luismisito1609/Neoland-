890b28747c6fe527d784b0a6a2121a739203ce87
Luismisito1609 <luismiguelgalanmunoz@gmail.com>
2024-04-08 17:39:39 +0200

    guia juegos

--- MODULO 1/2.Functions/Iteracion8.js
+++ MODULO 1/2.Functions/Iteracion8.js
@@ -1,20 +1,26 @@
-// Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.  
+// Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
 // Puedes usar este array para probar tu función:
 const counterWords = [
-  'code',
-  'repeat',
-  'eat',
-  'sleep',
-  'code',
-  'enjoy',
-  'sleep',
-  'code',
-  'enjoy',
-  'upgrade',
-  'code'
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
 ];
-const contadorPalabras= (Words)=> (
-let (contadorPalabrasMapeo)= {}
 
-return 
-    )
+// const contadorPalabras= (Words)=> {
+// let contadorPalabrasMapeo= {};
+// Words.forEach ((palabra)=>
+// if (palabra in contadorPalabrasMapeo)
+// {contadorPalabrasMapeo[palabra] +=1};
+// else {
+
+// }
+
+// return }
